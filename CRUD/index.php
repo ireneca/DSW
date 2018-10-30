64eb3a5bb5a8552746e910a6db05cf2e95a70cc5
ireneca <32276508+ireneca@users.noreply.github.com>
2018-10-30 22:13:32 +0200

    Update index.php

--- CRUD/index.php
+++ CRUD/index.php
@@ -12,8 +12,50 @@ $pdo=conectar();
 <body>
   <?php
     if (isset($_SESSION['name'])){
-      $sql = "SELECT * FROM autos";
-      $resultado = $pdo->query($sql);
+      $where = "";
+      if(isset($_POST['buscar'])){
+        function test_input($data) {
+          $data = trim($data);//Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
+          $data = stripslashes($data);//Devuelve un string con las barras invertidas retiradas. (\' se convierte en ') Barras invertidas dobles pasa a sencilla
+          $data = strip_tags($data);//Retira las etiquetas HTML y PHP de un string
+          $data = htmlspecialchars($data);//Convierte caracteres especiales en entidades HTML
+          return $data;
+        }
+        $marca = test_input($_POST['marca']);
+        $modelo = test_input($_POST['modelo']);
+        $ano = test_input($_POST['ano']);
+        $kilometros = test_input($_POST['kilometros']);
+        $execute = [];
+        if ($marca != "") {
+          $where = " WHERE make LIKE CONCAT('%',:marca,'%')";
+          $execute[':marca']=$marca;
+        }
+        if ($modelo != ""){
+          if ($where == "") $where = " WHERE model LIKE CONCAT('%',:model,'%')";
+          else $where .= " AND model LIKE CONCAT('%',:model,'%')";
+          $execute[':model']=$modelo;
+        }
+        if ($ano != ""){
+          if ($where == "") $where = " WHERE year LIKE CONCAT('%',:ano,'%')";
+          else $where .= " AND year LIKE CONCAT('%',:ano,'%')";
+          $execute[':ano']=$ano;
+        }
+        if ($kilometros != ""){
+          if ($where == "") $where = " WHERE mileage LIKE CONCAT('%',:kilometros,'%')";
+          else $where .= " AND mileage LIKE CONCAT('%',:kilometros,'%')";
+          $execute[':kilometros']=$kilometros;
+        }
+        if($where != ""){
+          $sql = "SELECT * FROM autos ".$where;
+          $resultado = $pdo->prepare($sql);
+          $resultado->execute($execute);
+        }
+      }
+      if($where == ""){
+        $sql = "SELECT * FROM autos ";
+        $resultado = $pdo->query($sql);
+      }
+      echo $sql;
       $numfilas= $resultado->rowCount();
       echo "<h2>Automoviles</h2>";
       if ( isset($_SESSION['error']) ) {
@@ -24,6 +66,16 @@ $pdo=conectar();
         echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
         unset($_SESSION['success']);
       }
+      ?>
+      <form method="post">
+        <input type="text" name="marca" placeholder="Marca">
+        <input type="text" name="modelo" placeholder="Modelo">
+        <input type="text" name="ano" placeholder="Año">
+        <input type="text" name="kilometros" placeholder="Kilometros">
+        <input type="submit" value="Buscar" name="buscar"/>
+      </form>
+      </br>
+      <?php
       echo "<table border='1'>
         <tr><th>Marca</th><th>Modelo</th><th>Año</th><th>Kilometros</th><th>Accion</th></tr>";
       if ($numfilas) {
